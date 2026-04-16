20e78b5cc1108ec14faef765f88db6f16ee51fd7
Alban Mouton <alban.mouton@gmail.com>
2026-04-16 19:36:59 +0200

    fix: failure in github CI test running

--- tests/state-teardown.ts
+++ tests/state-teardown.ts
@@ -4,5 +4,9 @@ import { test as teardown } from '@playwright/test'
 teardown('Stateful tests teardown', () => {
   const pid = process.env.TAIL_PID
   assert.ok(pid, 'Tail process PID is not defined')
-  process.kill(parseInt(pid))
+  try {
+    process.kill(parseInt(pid))
+  } catch (err: any) {
+    if (err.code !== 'ESRCH') throw err
+  }
 })
